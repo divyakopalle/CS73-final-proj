9acca136f9318b8a67bd3ad7b5539c68ecea9f86
Hanlin Wang <allegrohanlin@gmail.com>
2020-11-16 13:32:05 -0800

    fixed edge cases

--- src/quilting.cpp
+++ src/quilting.cpp
@@ -5,23 +5,33 @@
 #include <iostream>
 #include <cstdlib>
 
+using namespace std;
+
 // random placement of blocks
 FloatImage quilt_random(const FloatImage &sample, int outsize, int patchsize) {
-    FloatImage output(outsize, outsize, sample.channels());
+
+    // Make sure patchsize is smaller than sample dimensions
+    if (patchsize > sample.width() || patchsize > sample.height()) {
+      cout << "Error: Patch size must be smaller the sample image dimensions." << endl;
+      exit (1);
+    }
+
+    FloatImage output(outsize, outsize, sample.channels()); // initialize output image
     srand(time(NULL)); // seed the random number generator
 
     // loop over output image by intervals of the patch size
     for (int x = 0; x <= output.width() - patchsize; x += patchsize) {
         for (int y = 0; y <= output.height() - patchsize; y += patchsize) {
-                // randomly pick a patch from sample image by defining its origin (top-left) pixel (making sure the patch won't go out of bands)
-                int patch_x0 = rand() % (sample.width() - patchsize + 1);
-                int patch_y0 = rand() % (sample.height() - patchsize + 1);
-            for (int c = 0; c < output.channels(); c++) {
 
-                // loop over the patch area in the output image and populate it with the corresponding pixels of the randomly sampled patch
-                for (int x1 = x; x1 < x + patchsize; x1++) {
-                    for (int y1 = y; y1 < y + patchsize; y1++) {
-                        output(x1, y1, c) = sample(patch_x0 + x1 - x, patch_y0 + y1 - y, c);
+            // randomly pick a patch from sample image by defining its origin (top-left) pixel (making sure the patch won't go out of bands)
+            int patch_x0 = rand() % (sample.width() - patchsize + 1);
+            int patch_y0 = rand() % (sample.height() - patchsize + 1);
+
+            // loop over the patch area in the output image and populate it with the corresponding pixels of the randomly sampled patch
+            for (int x1 = x; x1 < x + patchsize; x1++) {
+                for (int y1 = y; y1 < y + patchsize; y1++) {
+                    for (int c = 0; c < output.channels(); c++) {
+                      output(x1, y1, c) = sample(patch_x0 + x1 - x, patch_y0 + y1 - y, c);
                     }
                 }
             }
--- src/quilting_main.cpp
+++ src/quilting_main.cpp
@@ -8,15 +8,18 @@ void testRandomQuilting()
 {
     FloatImage stones_sample(DATA_DIR "/input/stones.png");
     FloatImage mat_sample(DATA_DIR "/input/mat.png");
+    FloatImage text_sample(DATA_DIR "/input/text.png");
     
     int outsize = 700;
     int patchsize = 100;
 
     FloatImage stones_random = quilt_random(stones_sample, outsize, patchsize);
     FloatImage mat_random = quilt_random(mat_sample, outsize, patchsize);
+    FloatImage text_random = quilt_random(text_sample, outsize, patchsize);
 
     stones_random.write(DATA_DIR "/output/stones_random_synthesis.png");
     mat_random.write(DATA_DIR "/output/mat_random_synthesis.png");
+    text_random.write(DATA_DIR "/output/text_random_synthesis.png");
 }
 
 // //test texture synthesis with simple quilting (overlapping method)
